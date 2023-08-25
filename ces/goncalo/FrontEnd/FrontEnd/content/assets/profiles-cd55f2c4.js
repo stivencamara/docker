f14f6884b785e236db698a899501e16ebce6766c
Stiven Câmara <stiven@outlook.com>
2023-08-25 11:43:10 +0100

    docker

---
+++ ces/goncalo/FrontEnd/FrontEnd/content/assets/profiles-cd55f2c4.js
@@ -0,0 +1,1 @@
+import{b as r}from"./index-074485c3.js";const s={search:e=>r.post("/profiles/search",e),create:e=>r.post("/profiles",e),update:e=>r.put("/profiles",e),getById:e=>r.get(`/profiles/${e}`),delete:e=>r.delete(`/profiles/${e}`)};export{s as p};
---
+++ ces/goncalo/FrontEnd/FrontEnd/content/assets/settings-c1957f95.js
@@ -0,0 +1,1 @@
+import{b as e}from"./index-074485c3.js";const t={getLanguages:()=>e.get("/settings/languages")};export{t as s};
