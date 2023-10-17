45e3845247142021185956cbf4a5746860a183ac
Stiven Câmara <stiven@outlook.com>
2023-10-17 14:53:00 +0100

    front

---
+++ ces/prd/frontend/frontend-1/content/assets/treeview-03bac8f4.js
@@ -0,0 +1,1 @@
+import{b as t}from"./index-af43a57b.js";const i={get:e=>t.post("/treeview/get",e),update:e=>t.put("/treeview/update",e),getIcons:(e="pt")=>t.get(`/treeview/icons/${e}`)};export{i as t};
