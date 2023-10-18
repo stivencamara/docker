6d0db80066fde8ceb34621c221f63e530ae2c78f
João da Conceição <joao_miguel_grilo@hotmail.com>
2023-10-18 10:15:59 +0100

    deploy qa

--- ces/goncalo/FrontEnd/FrontEnd/content/assets/profiles-8885b839.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./index-c417b10a.js";const s={search:e=>r.post("/profiles/search",e),create:e=>r.post("/profiles",e),update:e=>r.put("/profiles",e),getById:e=>r.get(`/profiles/${e}`),delete:e=>r.delete(`/profiles/${e}`)};export{s as p};
