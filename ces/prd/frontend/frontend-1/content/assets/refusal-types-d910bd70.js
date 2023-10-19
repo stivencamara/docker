f5e465e3c604a04f10e70d451fd1b1b76077113c
Stiven Câmara <stiven@outlook.com>
2023-10-19 17:59:38 +0100

    prd

---
+++ ces/prd/frontend/frontend-1/content/assets/refusal-types-d910bd70.js
@@ -0,0 +1,1 @@
+import{b as t}from"./index-80ba5b4e.js";const r={search:e=>t.post("/settings/refusaltype/search",e),create:e=>t.post("/settings/refusaltype",e),update:e=>t.put("/settings/refusaltype",e),delete:e=>t.delete(`/settings/refusaltype/${e}`),getById:e=>t.get(`/settings/refusaltype/${e}`)};export{r};
