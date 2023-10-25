f2a07e985d12695ffc2ed3405417b22431a7717a
João da Conceição <joao_miguel_grilo@hotmail.com>
2023-10-25 09:52:42 +0100

    deploy

--- ces/goncalo/FrontEnd/FrontEnd/content/assets/treeview-36483d37.js
+++
@@ -1,1 +0,0 @@
-import{b as t}from"./index-eb4a79b4.js";const i={get:e=>t.post("/treeview/get",e),update:e=>t.put("/treeview/update",e),getIcons:(e="pt")=>t.get(`/treeview/icons/${e}`)};export{i as t};
