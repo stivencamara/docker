2cd64c03b8e94886a8e46c29986a24754fef9a97
João da Conceição <joao_miguel_grilo@hotmail.com>
2023-11-14 09:51:33 +0000

    deploy qa

---
+++ ces/goncalo/FrontEnd/FrontEnd/content/assets/treeview-21321f2d.js
@@ -0,0 +1,1 @@
+import{b as t}from"./index-6321ead1.js";const p={get:e=>t.post("/treeview/get",e),update:e=>t.put("/treeview/update",e),getIcons:(e="pt")=>t.get(`/treeview/icons/${e}`),updateOrder:e=>t.put("/treeview/updateOrder",e)};export{p as t};
